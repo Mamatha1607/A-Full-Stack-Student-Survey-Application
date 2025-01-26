ba3102936b8ece47ac7dab81df187a8f197fa3a8
Mamatha1607 <156241927+Mamatha1607@users.noreply.github.com>
2025-01-26 15:35:26 -0500

    Update app.routes.ts

--- student-survey/src/app/app.routes.ts
+++ student-survey/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-// Authors: Mamatha Iruvaram and Adilakshmi Pratyusha Mothadaka
+// Author: Mamatha Iruvaram 
 //Description: This Angular AppRoutingModule defines and manages the application's routing configuration. It maps specific paths ('home', 'survey', 'surveys') to their respective components and sets a default route that redirects to the SurveyFormComponent when the root path ('') is accessed. It uses RouterModule.forRoot() to register the routes and exports RouterModule for use in other modules
 import { Routes } from '@angular/router';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
